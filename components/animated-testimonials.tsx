ba3dfcfd51515455c473e073af8d3a24428c585b
aditya1232121 <139427619+aditya1232121@users.noreply.github.com>
2024-12-18 19:17:18 +0530

    Update animated-testimonials.tsx

--- components/animated-testimonials.tsx
+++ components/animated-testimonials.tsx
@@ -38,53 +38,51 @@ export const AnimatedTestimonials = ({ testimonials, autoplay = false }) => {
   return (
     <div className="min-h-screen bg-white flex items-center justify-center">
       <div className="relative max-w-sm md:max-w-4xl mx-auto antialiased font-sans px-4 md:px-8 lg:px-12 py-20">
-        <div className="relative grid grid-cols-1 md:grid-cols-2 gap-20">
-          <div>
-            <div className="relative h-80 w-full">
-              <AnimatePresence>
-                {testimonials.map((testimonial, index) => (
-                  <motion.div
-                    key={testimonial.src}
-                    initial={{
-                      opacity: 0,
-                      scale: 0.9,
-                      z: -100,
-                      rotate: randomRotation ?? 0, // Use rotation only when it's set
-                    }}
-                    animate={{
-                      opacity: isActive(index) ? 1 : 0.7,
-                      scale: isActive(index) ? 1 : 0.95,
-                      z: isActive(index) ? 0 : -100,
-                      rotate: isActive(index) ? 0 : randomRotation ?? 0, // Use rotation only when it's set
-                      zIndex: isActive(index)
-                        ? 999
-                        : testimonials.length + 2 - index,
-                      y: isActive(index) ? [0, -80, 0] : 0,
-                    }}
-                    exit={{
-                      opacity: 0,
-                      scale: 0.9,
-                      z: 100,
-                      rotate: randomRotation ?? 0, // Use rotation only when it's set
-                    }}
-                    transition={{
-                      duration: 0.4,
-                      ease: "easeInOut",
-                    }}
-                    className="absolute inset-0 origin-bottom"
-                  >
-                    <Image
-                      src={testimonial.src}
-                      alt={testimonial.name}
-                      width={500}
-                      height={500}
-                      draggable={false}
-                      className="h-full w-full rounded-3xl object-cover object-center"
-                    />
-                  </motion.div>
-                ))}
-              </AnimatePresence>
-            </div>
+        <div className="relative grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-20">
+          <div className="relative h-80 md:h-full">
+            <AnimatePresence>
+              {testimonials.map((testimonial, index) => (
+                <motion.div
+                  key={testimonial.src}
+                  initial={{
+                    opacity: 0,
+                    scale: 0.9,
+                    z: -100,
+                    rotate: randomRotation ?? 0, // Use rotation only when it's set
+                  }}
+                  animate={{
+                    opacity: isActive(index) ? 1 : 0.7,
+                    scale: isActive(index) ? 1 : 0.95,
+                    z: isActive(index) ? 0 : -100,
+                    rotate: isActive(index) ? 0 : randomRotation ?? 0, // Use rotation only when it's set
+                    zIndex: isActive(index)
+                      ? 999
+                      : testimonials.length + 2 - index,
+                    y: isActive(index) ? [0, -80, 0] : 0,
+                  }}
+                  exit={{
+                    opacity: 0,
+                    scale: 0.9,
+                    z: 100,
+                    rotate: randomRotation ?? 0, // Use rotation only when it's set
+                  }}
+                  transition={{
+                    duration: 0.4,
+                    ease: "easeInOut",
+                  }}
+                  className="absolute inset-0 origin-bottom"
+                >
+                  <Image
+                    src={testimonial.src}
+                    alt={testimonial.name}
+                    width={500}
+                    height={500}
+                    draggable={false}
+                    className="h-full w-full rounded-3xl object-cover object-center"
+                  />
+                </motion.div>
+              ))}
+            </AnimatePresence>
           </div>
           <div className="flex justify-between flex-col py-4">
             <motion.div
@@ -137,13 +135,13 @@ export const AnimatedTestimonials = ({ testimonials, autoplay = false }) => {
             <div className="flex gap-4 pt-12 md:pt-0">
               <button
                 onClick={handlePrev}
-                className="h-7 w-7 rounded-full bg-gray-100 flex items-center justify-center group/button"
+                className="h-8 w-8 rounded-full bg-gray-100 flex items-center justify-center group/button"
               >
                 <IconArrowLeft className="h-5 w-5 text-black group-hover/button:rotate-12 transition-transform duration-300" />
               </button>
               <button
                 onClick={handleNext}
-                className="h-7 w-7 rounded-full bg-gray-100 flex items-center justify-center group/button"
+                className="h-8 w-8 rounded-full bg-gray-100 flex items-center justify-center group/button"
               >
                 <IconArrowRight className="h-5 w-5 text-black group-hover/button:-rotate-12 transition-transform duration-300" />
               </button>
