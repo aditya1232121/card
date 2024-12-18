b73c88266eb8150f42911e550de47234f3a5eb6f
Aditya <adityadhyani982@gmail.com>
2024-12-18 19:13:50 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  images: {
+    domains: [
+      "images.unsplash.com", // Add other domains as needed
+    ],
+  },
 };
-
-export default nextConfig;
